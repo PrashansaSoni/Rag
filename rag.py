62deab35cf539dcb588eb0097ad18a456c9e016f
Bhushan Bharat <97097381+john221wick@users.noreply.github.com>
2024-08-07 20:58:23 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -4,6 +4,15 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
+# Install these packages through pip
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+
+# If you want to use llama3 embeddings 
+ollama_embeddings = OllamaEmbeddings(model="llama3")
+
+# If you want to use other embeddings like 
+other_embeddings = SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2")
 
 pdfloader = PyPDFLoader("/home/prashansa-soni/Downloads/It ends with us.pdf")
 pdfpages = pdfloader.load_and_split()
@@ -17,10 +26,10 @@ for page in pdfpages:
     split_docs.extend(text_splitter.split_text(page.page_content))
 
 
-
+# Remove this line and use the above lines instead
 embeddings = OpenAIEmbeddings( )
 
-
+# Change the embeddings function name below to as defined earlier, ie ollama_embeddings or other_embeddings
 vector_store = FAISS.from_texts(split_docs, embeddings)
 
 
